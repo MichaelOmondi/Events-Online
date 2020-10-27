5c1c156fa014847c5d1b3e19343910cffbd9186b
MichaelOmondi <omondimike11@gmail.com>
2020-10-27 18:26:44 +0300

    create and add the admin details

--- Events/urls.py
+++ Events/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('Online.urls')),
 ]
